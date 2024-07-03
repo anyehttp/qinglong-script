eb2d756609895400ddab96f21620dc0ffbd07ff9
anyehttp <93598563+anyehttp@users.noreply.github.com>
2024-07-03 23:32:47 +0800

    Create AppleStore-AppPrice.py

---
+++ python/AppleStore-AppPrice.py
@@ -0,0 +1,55 @@
+import requests
+import os
+from bs4 import BeautifulSoup
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36 Edg/125.0.0.0',
+}
+
+def request_url(url):
+    with requests.Session() as session:
+        url_html = session.get(url, headers=headers)
+        url_html.encoding = 'utf-8'
+        return url_html
+
+def BeautifulSoup_html(price):
+    soup = BeautifulSoup(price.text, 'html.parser')
+    price = soup.find('li', class_='inline-list__item inline-list__item--bulleted app-header__list__item--price')
+    return price
+
+
+def bark(notice):
+
+    requests.get(f'https://api.day.app/5keujV8LLqpBhDTGMnUSD7/{notice}')
+
+
+
+def main():
+    # 读取环境变量中的app数据，如果没有设置则使用默认值
+    # app_数据 = os.getenv('app_data')
+    app_data = os.getenv('app_data', '{"地区":["cn", "us"], "名字":["pix站助手-精美二次元壁纸采集工具", "code-app"], "id":[1161125462, 1512938504]}')
+    
+    app_数据 = eval(app_data)  # 使用eval解析字符串成字典
+
+    assert len(app_数据['地区']) == len(app_数据['名字']) == len(app_数据['id'])
+    
+    notice = ''
+
+    for 地区, 名字, id in zip(app_数据['地区'], app_数据['名字'], app_数据['id']):
+
+        url = f'https://apps.apple.com/{地区}/app/{名字}/id{id}'
+
+        url_html = request_url(url)
+
+        BeautifulSoup_price = BeautifulSoup_html(url_html)
+
+        for i in BeautifulSoup_price:
+            print(名字, i.text.strip())
+            notice_bark = f"{名字} {i.text.strip()}"
+            notice += notice_bark + '\n'
+            
+    #调用通知函数
+    bark(notice)
+
+if __name__ == '__main__':
+    main()
